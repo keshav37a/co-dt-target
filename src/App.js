f261e7f2d998505b2a9896b738bdf722d505d20b
keshav <keshav.mathur@toppr.com>
2021-07-16 12:58:06 +0530

    small change

--- src/App.js
+++ src/App.js
@@ -2,39 +2,27 @@ import { useEffect } from "react";
 import "./App.css";
 
 function App() {
-  // useEffect(() => {
-  //   window.addEventListener(
-  //     "message",
-  //     (event) => {
-  //       if (event.origin !== "https://co-dt-source.web.app/") {
-  //         alert("Intercepted a message from unknown source");
-  //         return;
-  //       }
-  //       console.log(event);
-  //     },
-  //     false
-  //   );
+  useEffect(() => {
+    window.addEventListener(
+      "message",
+      (event) => {
+        console.log(event);
+        if (event.origin !== "https://co-dt-source.web.app") {
+          alert("Intercepted a message from unknown source");
+          return;
+        }
+        alert(
+          `message recieved - ClientId: ${event.data.ClientId}, ClientSecret:${event.data.ClientSecret}, User-profile-id: ${event.data["User-profile-id"]}`
+        );
+        console.log(event);
+      },
+      false
+    );
 
-  //   /* return () => {
-  //     window.removeEventListener("message");
-  //   }; */
-  // }, []);
-
-  window.addEventListener(
-    "message",
-    (event) => {
-      console.log(event);
-      if (event.origin !== "https://co-dt-source.web.app") {
-        alert("Intercepted a message from unknown source");
-        return;
-      }
-      alert(
-        `message recieved - ClientId: ${event.data.ClientId}, ClientSecret:${event.data.ClientSecret}, User-profile-id: ${event.data["User-profile-id"]}`
-      );
-      console.log(event);
-    },
-    false
-  );
+    return () => {
+      window.removeEventListener("message");
+    };
+  }, []);
 
   return (
     <div className="App">
