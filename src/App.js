7288fc422cffadd4620d75e4d14884f16be770eb
keshav <keshav.mathur@toppr.com>
2021-07-16 12:12:26 +0530

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import "./App.css";
 
 function App() {
+  useEffect(() => {
+    window.addEventListener(
+      "message",
+      (event) => {
+        if (event.origin !== "https://co-dt-source.web.app/") {
+          alert("Intercepted a message from unknown source");
+          return;
+        }
+        console.log(event);
+      },
+      false
+    );
+
+    return () => {
+      window.removeEventListener("message");
+    };
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Testing Cross Origin Data Transfer - Target</p>
       </header>
     </div>
   );
