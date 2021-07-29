4276a6e73c7a97c78f96cef7bf0fadbffa183012
keshav <keshav.mathur@toppr.com>
2021-07-29 19:29:31 +0530

    small change

--- src/App.js
+++ src/App.js
@@ -3,17 +3,12 @@ import cogoToast from "cogo-toast";
 import "./App.css";
 
 function App() {
-  console.log("testing");
   useEffect(() => {
     if (window?.opener) {
-      console.log("event listener added");
       window.opener.postMessage("target loaded", "*");
       window.addEventListener(
         "message",
         (event) => {
-          console.log("message recieved");
-          console.log(event.data);
-          console.log(event.data);
           if (event.origin === "https://co-dt-src.web.app") {
             cogoToast.success(`data recieved - ${JSON.stringify(event.data)}`);
           }
